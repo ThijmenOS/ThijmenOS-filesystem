a5e0cdb930e68588248ce5548f37884949676c22
Thijmen Brand <thijmen@ik.nu>
2022-11-11 16:21:35 +0100

    got rid of axios

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,34 @@
-import axios from "axios";
 import { host } from "@thijmenos/common";
 
-const api = axios.create({
-  baseURL: host,
-  timeout: 1000,
-});
+interface ApiOptions {
+  baseURL: string;
+}
+class Api {
+  private readonly _baseURL: string;
+
+  constructor(props: ApiOptions) {
+    this._baseURL = props.baseURL;
+  }
+
+  public async get(url: string) {
+    const response = await fetch(url);
+
+    return await response.json();
+  }
+
+  public async post(url: string, data: object | string) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return response.json();
+  }
+}
+
+const api = new Api({ baseURL: host });
 
 export default api;
